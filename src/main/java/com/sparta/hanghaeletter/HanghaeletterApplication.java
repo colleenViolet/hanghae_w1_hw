4377c09704721c57436c54b18862388164b1ef3f
DESKTOP-380TO0D\ktc system <ledgy98@gmail.com>
2023-02-04 03:21:21 +0900

    Initial commit

---
+++ src/main/java/com/sparta/hanghaeletter/HanghaeletterApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.hanghaeletter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanghaeletterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanghaeletterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/hanghaeletter/HanghaeletterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.hanghaeletter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanghaeletterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
